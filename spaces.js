fa8b14c508aca5cef1d0f057909ceac69ca20676
Quinten Clause <quinten.clause@studioemma.eu>
2018-02-06 00:19:24 +0100

    Some code sketching

--- spaces.js
+++ spaces.js
@@ -11,8 +11,34 @@ window.onload = () => {
 
         engine.scene = buildingJson.doors.map( door => Quad(door) );
 
-        engine.camera.x = 2.5;
-        engine.camera.z = 2.5;
+        let waypoints = buildingJson.waypoints;
+        let w = 0;
+        let from = waypoints[w];
+        w = 1;
+        let to = waypoints[w];
+        let angle = Math.atan2(to.x - from.x, to.z - from.z) * 180 / Math.PI;
+
+        console.log(angle);
+
+        engine.camera.x = from.x;
+        engine.camera.z = from.z;
+        engine.camera.rotationY = angle;
+
+        engine.camera.step = function () {
+            engine.camera.x += (to.x - from.x) / 240;
+            engine.camera.z += (to.z - from.z) / 240;
+            engine.camera.rotationY += (angle - engine.camera.rotationY) / 6;
+            if (Math.abs(to.x - engine.camera.x) < .1 && Math.abs(to.z - engine.camera.z) < .1) {
+                w++;
+                if (w >= waypoints.length) {
+                    w = 0;
+                }
+                from = to;
+                to = waypoints[w];
+                angle = Math.atan2(to.x - from.x, to.z - from.z) * 180 / Math.PI;
+                console.log(angle);
+            }
+        };
 
         engine.start();
 
